97a878f1af3dabb3cb036f2e39dcbdc49fd04eff
Severin Bergsmann <severin.bergsmann@scch.at>
2024-11-27 15:28:15 +0100

    added rich chat message

--- bot/models/chat.py
+++ bot/models/chat.py
@@ -6,9 +6,62 @@ from pydantic import BaseModel, Field
 
 
 class ChatMessage(BaseModel):
+    id: int
     user: str
     message: str
     message_timedelta: TimeDelta
+
+    def __str__(self) -> str:
+        return "[#{id}] {user} (after {time}s): {message}".format(
+            id=self.id,
+            user=self.user,
+            time=self.message_timedelta.seconds,
+            message=self.message
+        )
+
+
+class RichChatMessage(BaseModel):
+    # base data
+    id: int
+    message: str
+    message_timedelta: TimeDelta
+
+    # enriched data
+    sender: str
+    receivers: List[str]
+    factual_information: str
+    self_revelation: str
+    relationship: str
+    appeal: str
+    linked_messages: List[int]
+
+    def __str__(self) -> str:
+        receivers: str = (
+            "- directed to: " + ", ".join(self.receivers)
+            if self.receivers else "directed to: unclear"
+        )
+        linked_to: str = (
+            "- linked to: " + "[" + ", ".join(["#" + str(id) for id in self.linked_messages]) + "]"
+            if self.linked_messages else ""
+        )
+        return (
+            """#{id} {sender} (after {time}s): {message}
+    {receivers}
+    - factual info: {factual_information}
+    - self-revelation: {self_revelation}
+    - appeal: {appeal}
+    {linked_to}""".format(
+                id=self.id,
+                sender=self.sender,
+                time=self.message_timedelta.seconds,
+                message=self.message,
+                receivers=receivers,
+                factual_information=self.factual_information,
+                self_revelation=self.self_revelation,
+                appeal=self.appeal,
+                linked_to=linked_to
+            )
+        )
 
 
 class Chat(BaseModel):
@@ -19,9 +72,9 @@ class Chat(BaseModel):
     player2: str
     bot: str
     language: str
-    messages: List[ChatMessage] = Field(default_factory=list)
+    messages: List[ChatMessage | RichChatMessage] = Field(default_factory=list)  # TODO: make it a dict
 
-    def add_message(self, message: ChatMessage) -> None:
+    def add_message(self, message: ChatMessage | RichChatMessage) -> None:
         self.messages.append(message)
 
     def get_message_timedelta(self, time: DateTime) -> TimeDelta:
@@ -30,9 +83,22 @@ class Chat(BaseModel):
     def update_last_message_time(self, time: DateTime) -> None:
         self.last_message_time = time
 
+    def get_formatted_chat_history(self) -> str:
+        messages: str = "\n".join([str(message) for message in self.messages])
+        start_time: str = "Start Time: " + self.start_time.strftime("%Y-%m-%d %H:%M:%S")
+        header: str = "#MessageId User (Seconds since last message): Message"
+        return f"{start_time}\n{header}\n{messages}"
+
+    def format_participants(self) -> str:
+        return f"{self.player1}, {self.player2}, {self.bot}"  # TODO perhaps shuffle the order
+
     @property
     def humans(self):
         return [self.player1, self.player2]
+
+    @property
+    def participants(self):
+        return [self.player1, self.player2, self.bot]
 
     @property
     def duration(self):
