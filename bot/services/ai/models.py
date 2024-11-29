40af516d9d35161179ee960d6ac39a413bf3075f
Severin Bergsmann <severin.bergsmann@scch.at>
2024-11-29 11:46:13 +0100

    added AI response models

---
+++ bot/services/ai/models.py
@@ -0,0 +1,39 @@
+""""""
+
+from typing import List
+from pydantic import BaseModel, Field
+
+from bot.services.ai import prompts
+
+
+class FourSidesAnalysis(BaseModel):
+    sender: str = Field(
+        description=prompts.FSA.SENDER
+    )
+    receivers: List[str] = Field(
+        description=prompts.FSA.RECEIVERS
+    )
+    factual_information: str = Field(
+        description=prompts.FSA.FACTUAL_INFORMATION
+    )
+    self_revelation: str = Field(
+        description=prompts.FSA.SELF_REVELATION
+    )
+    relationship: str = Field(
+        description=prompts.FSA.RELATIONSHIP
+    )
+    appeal: str = Field(
+        description=prompts.FSA.APPEAL
+    )
+    linked_messages: List[int] = Field(
+        description=prompts.FSA.LINKED_MESSAGES
+    )
+
+
+class BotResponse(BaseModel):
+    user: str = Field(
+        description="The user who sent the message."
+    )
+    message: str | None = Field(
+        description="The message to send based on the given chat history."
+    )
